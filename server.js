6d21da8c1ddb1dc06a3fd26a3f35d42c0497a9f8
Demivdm <demi.van.der.maarl@hva.nl>
2023-06-14 08:22:23 +0200

    uninstall js libraries
    uninstalled js libraries like chart.js and node-canvas

--- server.js
+++ server.js
@@ -3,7 +3,6 @@
 import * as path from 'path'
 import { createServer } from 'http'
 import express, { response } from 'express'
-import { Chart } from 'chart'
  
 const app = express()
 const http = createServer(app)
