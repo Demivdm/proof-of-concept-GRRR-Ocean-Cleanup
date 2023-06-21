1f5ab6730e703d80f238b49366939cc47e27715e
Demivdm <demi.van.der.maarl@hva.nl>
2023-06-21 15:40:01 +0200

    added final colors

--- server.js
+++ server.js
@@ -58,41 +58,169 @@ app.get("/interceptor-001", (request, response) => {
 });
 
 app.get("/interceptor-002", (request, response) => {
-  response.render("interceptor-002");
-});
+  // json bestand wordt opgehaald uit de variabele
+  fetchJson(dashboardUrl).then((data) => {
+    // een variabel met enriched data erin waarbij data.systems worden samengevoegd
+    // In map() maak ik het item "interceptor" aan
+    const enrichedData = data.systems.map((interceptor) => {
+      // Aan dit item geef ik het object met de null waarde aan
+      // vervolgens roep ik de standaard funtie getRandom.. aan en defenier ik het minimum en maximum getal.
+      interceptor["debris_extracted_last_30d"] = getRandomIntInclusive(0, 1000);
+      interceptor["debris_extracted_total"] = getRandomIntInclusive(1000, 5000);
+      //   Daarna wordt het item interceptor teruggestuurd met de nieuwe waarden teruggestuurd
+      return interceptor;
+    });
 
+    //   pagina wordt gerenderd met de data erop
+    response.render("interceptor-002", { data: enrichedData });
+  });
+});
 app.get("/interceptor-003", (request, response) => {
-  response.render("interceptor-003");
-});
+  // json bestand wordt opgehaald uit de variabele
+  fetchJson(dashboardUrl).then((data) => {
+    // een variabel met enriched data erin waarbij data.systems worden samengevoegd
+    // In map() maak ik het item "interceptor" aan
+    const enrichedData = data.systems.map((interceptor) => {
+      // Aan dit item geef ik het object met de null waarde aan
+      // vervolgens roep ik de standaard funtie getRandom.. aan en defenier ik het minimum en maximum getal.
+      interceptor["debris_extracted_last_30d"] = getRandomIntInclusive(0, 1000);
+      interceptor["debris_extracted_total"] = getRandomIntInclusive(1000, 5000);
+      //   Daarna wordt het item interceptor teruggestuurd met de nieuwe waarden teruggestuurd
+      return interceptor;
+    });
 
+    //   pagina wordt gerenderd met de data erop
+    response.render("interceptor-003", { data: enrichedData });
+  });
+});
 app.get("/interceptor-004", (request, response) => {
-  response.render("interceptor-004");
-});
+  // json bestand wordt opgehaald uit de variabele
+  fetchJson(dashboardUrl).then((data) => {
+    // een variabel met enriched data erin waarbij data.systems worden samengevoegd
+    // In map() maak ik het item "interceptor" aan
+    const enrichedData = data.systems.map((interceptor) => {
+      // Aan dit item geef ik het object met de null waarde aan
+      // vervolgens roep ik de standaard funtie getRandom.. aan en defenier ik het minimum en maximum getal.
+      interceptor["debris_extracted_last_30d"] = getRandomIntInclusive(0, 1000);
+      interceptor["debris_extracted_total"] = getRandomIntInclusive(1000, 5000);
+      //   Daarna wordt het item interceptor teruggestuurd met de nieuwe waarden teruggestuurd
+      return interceptor;
+    });
 
+    //   pagina wordt gerenderd met de data erop
+    response.render("interceptor-004", { data: enrichedData });
+  });
+});
 app.get("/interceptor-005", (request, response) => {
-  response.render("interceptor-005");
-});
+  // json bestand wordt opgehaald uit de variabele
+  fetchJson(dashboardUrl).then((data) => {
+    // een variabel met enriched data erin waarbij data.systems worden samengevoegd
+    // In map() maak ik het item "interceptor" aan
+    const enrichedData = data.systems.map((interceptor) => {
+      // Aan dit item geef ik het object met de null waarde aan
+      // vervolgens roep ik de standaard funtie getRandom.. aan en defenier ik het minimum en maximum getal.
+      interceptor["debris_extracted_last_30d"] = getRandomIntInclusive(0, 1000);
+      interceptor["debris_extracted_total"] = getRandomIntInclusive(1000, 5000);
+      //   Daarna wordt het item interceptor teruggestuurd met de nieuwe waarden teruggestuurd
+      return interceptor;
+    });
 
+    //   pagina wordt gerenderd met de data erop
+    response.render("interceptor-005", { data: enrichedData });
+  });
+});
 app.get("/interceptor-006", (request, response) => {
-  response.render("interceptor-006");
-});
+  // json bestand wordt opgehaald uit de variabele
+  fetchJson(dashboardUrl).then((data) => {
+    // een variabel met enriched data erin waarbij data.systems worden samengevoegd
+    // In map() maak ik het item "interceptor" aan
+    const enrichedData = data.systems.map((interceptor) => {
+      // Aan dit item geef ik het object met de null waarde aan
+      // vervolgens roep ik de standaard funtie getRandom.. aan en defenier ik het minimum en maximum getal.
+      interceptor["debris_extracted_last_30d"] = getRandomIntInclusive(0, 1000);
+      interceptor["debris_extracted_total"] = getRandomIntInclusive(1000, 5000);
+      //   Daarna wordt het item interceptor teruggestuurd met de nieuwe waarden teruggestuurd
+      return interceptor;
+    });
 
+    //   pagina wordt gerenderd met de data erop
+    response.render("interceptor-006", { data: enrichedData });
+  });
+});
 app.get("/interceptor-007", (request, response) => {
-  response.render("interceptor-007");
-});
+  // json bestand wordt opgehaald uit de variabele
+  fetchJson(dashboardUrl).then((data) => {
+    // een variabel met enriched data erin waarbij data.systems worden samengevoegd
+    // In map() maak ik het item "interceptor" aan
+    const enrichedData = data.systems.map((interceptor) => {
+      // Aan dit item geef ik het object met de null waarde aan
+      // vervolgens roep ik de standaard funtie getRandom.. aan en defenier ik het minimum en maximum getal.
+      interceptor["debris_extracted_last_30d"] = getRandomIntInclusive(0, 1000);
+      interceptor["debris_extracted_total"] = getRandomIntInclusive(1000, 5000);
+      //   Daarna wordt het item interceptor teruggestuurd met de nieuwe waarden teruggestuurd
+      return interceptor;
+    });
 
+    //   pagina wordt gerenderd met de data erop
+    response.render("interceptor-007", { data: enrichedData });
+  });
+});
 app.get("/interceptor-008", (request, response) => {
-  response.render("interceptor-008");
+  // json bestand wordt opgehaald uit de variabele
+  fetchJson(dashboardUrl).then((data) => {
+    // een variabel met enriched data erin waarbij data.systems worden samengevoegd
+    // In map() maak ik het item "interceptor" aan
+    const enrichedData = data.systems.map((interceptor) => {
+      // Aan dit item geef ik het object met de null waarde aan
+      // vervolgens roep ik de standaard funtie getRandom.. aan en defenier ik het minimum en maximum getal.
+      interceptor["debris_extracted_last_30d"] = getRandomIntInclusive(0, 1000);
+      interceptor["debris_extracted_total"] = getRandomIntInclusive(1000, 5000);
+      //   Daarna wordt het item interceptor teruggestuurd met de nieuwe waarden teruggestuurd
+      return interceptor;
+    });
+
+    //   pagina wordt gerenderd met de data erop
+    response.render("interceptor-008", { data: enrichedData });
+  });
 });
 
 app.get("/interceptor-009", (request, response) => {
-  response.render("interceptor-009");
+  // json bestand wordt opgehaald uit de variabele
+  fetchJson(dashboardUrl).then((data) => {
+    // een variabel met enriched data erin waarbij data.systems worden samengevoegd
+    // In map() maak ik het item "interceptor" aan
+    const enrichedData = data.systems.map((interceptor) => {
+      // Aan dit item geef ik het object met de null waarde aan
+      // vervolgens roep ik de standaard funtie getRandom.. aan en defenier ik het minimum en maximum getal.
+      interceptor["debris_extracted_last_30d"] = getRandomIntInclusive(0, 1000);
+      interceptor["debris_extracted_total"] = getRandomIntInclusive(1000, 5000);
+      //   Daarna wordt het item interceptor teruggestuurd met de nieuwe waarden teruggestuurd
+      return interceptor;
+    });
+
+    //   pagina wordt gerenderd met de data erop
+    response.render("interceptor-009", { data: enrichedData });
+  });
 });
 
 app.get("/interceptor-010", (request, response) => {
-  response.render("interceptor-010");
-});
+  // json bestand wordt opgehaald uit de variabele
+  fetchJson(dashboardUrl).then((data) => {
+    // een variabel met enriched data erin waarbij data.systems worden samengevoegd
+    // In map() maak ik het item "interceptor" aan
+    const enrichedData = data.systems.map((interceptor) => {
+      // Aan dit item geef ik het object met de null waarde aan
+      // vervolgens roep ik de standaard funtie getRandom.. aan en defenier ik het minimum en maximum getal.
+      interceptor["debris_extracted_last_30d"] = getRandomIntInclusive(0, 1000);
+      interceptor["debris_extracted_total"] = getRandomIntInclusive(1000, 5000);
+      //   Daarna wordt het item interceptor teruggestuurd met de nieuwe waarden teruggestuurd
+      return interceptor;
+    });
 
+    //   pagina wordt gerenderd met de data erop
+    response.render("interceptor-010", { data: enrichedData });
+  });
+});
 // Start een http server op het ingestelde poortnummer en log de url
 http.listen(port, () => {
   console.log("listening on http://localhost:" + port);
